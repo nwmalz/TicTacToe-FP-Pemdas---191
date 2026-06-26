bcdec1fd35fcbb3b4dc78a722340e09c10dc4426
nwmalz <akmal.alifanri2007@gmail.com>
2026-06-26 20:19:18 +0700

    DatabaseManager.java

--- TicTacToe FP Pemdas/src/DatabaseManager.java
+++ TicTacToe FP Pemdas/src/DatabaseManager.java
@@ -5,9 +5,9 @@ import java.sql.SQLException;
 public class DatabaseManager {
 
     // TODO: Ganti PASSWORD dengan password PostgreSQL kamu
-    private static final String URL      = "jdbc:postgresql://localhost:5433/game_project";
+    private static final String URL      = "jdbc:postgresql://localhost:54333/game_project";
     private static final String USER     = "postgres";
-    private static final String PASSWORD = "akmal01073123";
+    private static final String PASSWORD = "password saya";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
